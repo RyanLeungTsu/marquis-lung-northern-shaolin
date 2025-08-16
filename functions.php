1585e91fb438182f30be3f48d61ab6dd74d067f6
RyanLeungTsu <ryanleungtsu@gmail.com>
2025-08-16 03:15:48 -0700

    rewrote entire functions.php to clean up all my earlier messy code, bundles enqueues and terms for cpts as well as CPT themselves and Custom taxonomies, fixed many errors on homepage due to enqueue conflicts on functions.php

--- functions.php
+++ functions.php
@@ -10,39 +10,33 @@
  */
 
 
-//  Enqueue Styles
-function enqueueStyles()
-{
-    // Enqueue theme toggle to ensure root colors work
-    wp_enqueue_style('ns-theme-toggle-style', get_template_directory_uri() . '/assets/css/theme-toggle.css', [], '1.0');
-    wp_enqueue_style('northernShaolinStyle', get_theme_file_uri('/style.css'), ['ns-theme-toggle-style'], '1.0');
-    wp_enqueue_style('ns-carousel-style', get_template_directory_uri() . '/assets/css/carousel.css', ['ns-theme-toggle-style'], '1.0');
-    wp_enqueue_style('ns-home-page-style', get_template_directory_uri() . '/assets/css/home.css', ['ns-theme-toggle-style'], '1.0');
-    wp_enqueue_style('ns-scroll-arrow-style', get_template_directory_uri() . '/assets/css/scroll-arrow.css', ['ns-theme-toggle-style'], '1.0');
-    wp_enqueue_style('ns-myCalendar-style', get_template_directory_uri() . '/assets/css/myCalendar.css', ['ns-theme-toggle-style'], '1.0');
-}
-add_action('wp_enqueue_scripts', 'enqueueStyles');
 
-// Block editor support
+
+//  Enqueue Styles
+function ns_enqueue_assets() {
+    // Main Styles
+    wp_enqueue_style('northernShaolinStyle', get_theme_file_uri('/style.css'), [], '1.0');
+    wp_enqueue_style('ns-theme-toggle', get_template_directory_uri() . '/assets/css/theme-toggle.css', [], '1.0');
+    wp_enqueue_style('ns-carousel-style', get_template_directory_uri() . '/assets/css/carousel.css', ['ns-theme-toggle'], '1.0');
+    wp_enqueue_style('ns-home-page-style', get_template_directory_uri() . '/assets/css/home.css', ['ns-theme-toggle'], '1.0');
+    wp_enqueue_style('ns-myCalendar-style', get_template_directory_uri() . '/assets/css/myCalendar.css', ['ns-theme-toggle'], '1.0');
+    wp_enqueue_style('ns-override', get_template_directory_uri() . '/assets/css/override.css', [], '1.0');
+    wp_enqueue_style('scroll-arrow-style', get_template_directory_uri() . '/assets/css/scroll-arrow.css', [], '1.0');
+
+    // Scripts
+    wp_enqueue_script('heroCarouselJs', get_template_directory_uri() . '/assets/js/carousel/carousel.js', [], null, true);
+    wp_enqueue_script('scroll-arrow-script', get_template_directory_uri() . '/assets/js/scroll-arrow/scroll-arrow.js', [], '1.0', true);
+    wp_enqueue_script('theme-toggle-script', get_template_directory_uri() . '/assets/js/theme-toggle/theme-toggle.js', [], '1.0', true);
+}
+add_action('wp_enqueue_scripts', 'ns_enqueue_assets');
+
+// Editor Styles
 add_theme_support('editor-styles');
 add_editor_style('/assets/css/editor.css');
 
-// Override css for overriding plugins and theme css
-function ns_enqueue_overrides() {
-    wp_enqueue_style(
-        'ns-override',
-        get_template_directory_uri() . '/assets/css/override.css',
-        [],
-        '1.0'
-    );
-}
-add_action('wp_enqueue_scripts', 'ns_enqueue_overrides', 99);
-
-// Makes ACF exposed in REST API
+// Expose ACF fields in REST API for Events
 function expose_acf_fields_in_rest($data, $post, $request) {
-    // Only for event CPT
     if ($post->post_type === 'event') {
-        // Get all ACF fields for this post
         $acf_fields = get_fields($post->ID);
         if ($acf_fields) {
             $data->data['acf'] = $acf_fields;
@@ -52,362 +46,194 @@ function expose_acf_fields_in_rest($data, $post, $request) {
 }
 add_filter('rest_prepare_event', 'expose_acf_fields_in_rest', 10, 3);
 
-// CUSTOM CPTS
-// Custom Post Type: Instructors
-function register_instructors_cpt()
-{
-    $args = array(
-        'labels' => array(
-            'name' => 'Instructors',
-            'singular_name' => 'Instructor',
-        ),
+// Custom Post Types
+function ns_register_cpts() {
+    // Instructors
+    register_post_type('instructor', [
+        'labels' => ['name' => 'Instructors','singular_name' => 'Instructor'],
         'public' => true,
         'has_archive' => true,
-        'rewrite' => array('slug' => 'instructors-archive'),
-        'supports' => array('title', 'editor', 'thumbnail'),
+        'rewrite' => ['slug' => 'instructors-archive'],
+        'supports' => ['title','editor','thumbnail'],
         'menu_position' => 5,
         'menu_icon' => 'dashicons-groups',
         'show_in_rest' => true,
-        'capability_type' => 'post',
-        'map_meta_cap' => true,
-    );
+    ]);
 
-    register_post_type('instructor', $args);
-}
-add_action('init', 'register_instructors_cpt');
-
-// Custom Post Type: Directors
-function registerDirectorsCpt()
-{
-    $args = array(
-        'labels' => array(
-            'name' => 'Directors',
-            'singular_name' => 'Director',
-        ),
+    // Directors
+    register_post_type('director', [
+        'labels' => ['name' => 'Directors','singular_name' => 'Director'],
         'public' => true,
         'has_archive' => true,
-        'rewrite' => array('slug' => 'directors-archive'),
-        'supports' => array('title', 'editor', 'thumbnail'),
+        'rewrite' => ['slug' => 'directors-archive'],
+        'supports' => ['title','editor','thumbnail'],
         'menu_position' => 5,
         'menu_icon' => 'dashicons-businessman',
         'show_in_rest' => true,
-    );
-    register_post_type('director', $args);
-}
-add_action('init', 'registerDirectorsCpt');
+    ]);
 
-// Custom Post Type: Gallery
-function registerGalleryCpt() {
-    $args = array(
-        'labels' => array(
-            'name' => 'Photos',
-            'singular_name' => 'Photo',
-        ),
+    // Photos
+    register_post_type('photo', [
+        'labels' => ['name' => 'Photos','singular_name' => 'Photo'],
         'public' => true,
         'has_archive' => true,
-        'rewrite' => array('slug' => 'photos'),
-        'supports' => array('title', 'editor', 'thumbnail'),
+        'rewrite' => ['slug' => 'photos'],
+        'supports' => ['title','editor','thumbnail'],
         'menu_position' => 6,
         'menu_icon' => 'dashicons-format-image',
-        'show_in_rest' => true, 
-    );
-    register_post_type('photo', $args);
-}
-add_action('init', 'registerGalleryCpt');
+        'show_in_rest' => true,
+    ]);
 
-// Custom Post Type: Events
-function registerEventsCpt() {
-    $args = array(
-        'labels' => array(
-            'name' => 'Events',
-            'singular_name' => 'Event',
-        ),
+    // Events
+    register_post_type('event', [
+        'labels' => ['name' => 'Events','singular_name' => 'Event'],
         'public' => true,
         'has_archive' => true,
-        'rewrite' => array('slug' => 'events'),
-        'supports' => array('title', 'editor', 'custom-fields'),
+        'rewrite' => ['slug' => 'events'],
+        'supports' => ['title','editor','custom-fields'],
         'menu_position' => 7,
         'menu_icon' => 'dashicons-calendar-alt',
         'show_in_rest' => true,
-    );
-    register_post_type('event', $args);
+    ]);
 }
-add_action('init', 'registerEventsCpt');
+add_action('init', 'ns_register_cpts');
 
-// CUSTOM TAXONOMY
-// Custom Taxonomy: Location
-function registerLocationTaxonomy()
-{
-    register_taxonomy('location', 'instructor', array(
-        'labels' => array(
-            'name' => 'Locations',
-            'singular_name' => 'Location',
-            'add_new_item' => 'Add New Location',
-            'new_item_name' => 'New Location Name',
-        ),
+// Custom Taxonomies
+function ns_register_taxonomies() {
+    // Location
+    register_taxonomy('location', 'instructor', [
+        'labels' => ['name'=>'Locations','singular_name'=>'Location','add_new_item'=>'Add New Location','new_item_name'=>'New Location Name'],
         'hierarchical' => true,
         'show_admin_column' => true,
-        'rewrite' => array('slug' => 'ns-location'),
+        'rewrite' => ['slug'=>'ns-location'],
         'show_in_rest' => true,
-    ));
+    ]);
 
-    $defaultLocations = array(
-        'Pinetree',
-        'Douglas Park',
-        'Britannia',
-        'Lochdale Hall',
-        'Killarney',
-        'Sifu'
-    );
+    // Status
+    register_taxonomy('ns-status', ['instructor','director'], [
+        'labels' => ['name'=>'Status','singular_name'=>'Status','add_new_item'=>'Add New Status','new_item_name'=>'New Status Name'],
+        'hierarchical' => true,
+        'show_admin_column' => true,
+        'rewrite' => ['slug'=>'ns-status'],
+        'show_in_rest' => true,
+    ]);
 
-    foreach ($defaultLocations as $location) {
-        if (!term_exists($location, 'location')) {
-            wp_insert_term($location, 'location');
-        }
-    }
+    // Director Title
+    register_taxonomy('director-title', 'director', [
+        'labels' => ['name'=>'Title','singular_name'=>'Title','add_new_item'=>'Add New Title','new_item_name'=>'New Title Name'],
+        'hierarchical' => true,
+        'show_admin_column' => true,
+        'rewrite' => ['slug'=>'director-title'],
+        'show_in_rest' => true,
+    ]);
+
+    // Gallery Filter
+    register_taxonomy('gallery-filter', 'photo', [
+        'labels' => ['name'=>'Photo Types','singular_name'=>'Photo Type','add_new_item'=>'Add New Photo Type','new_item_name'=>'New Photo Type Name'],
+        'hierarchical' => true,
+        'show_admin_column' => true,
+        'rewrite' => ['slug'=>'gallery-filter'],
+        'show_in_rest' => true,
+    ]);
 }
-add_action('init', 'registerLocationTaxonomy');
+add_action('init', 'ns_register_taxonomies');
 
-// Custom fix for excluding post
-function exclude_specific_post_from_queries($query) {
+// Default Terms (Locations, Status, Director Title, Gallery Filter)
+function ns_add_terms() {
+    $locations = ['Pinetree','Douglas Park','Britannia','Lochdale Hall','Killarney','Sifu'];
+    foreach ($locations as $loc) if (!term_exists($loc,'location')) wp_insert_term($loc,'location');
+
+    $statuses = ['Current','Former','Student Instructor'];
+    foreach ($statuses as $status) if (!term_exists($status,'ns-status')) wp_insert_term($status,'ns-status');
+
+    $titles = ['President','Vice President','Treasurer','Secretary','Member'];
+    foreach ($titles as $title) if (!term_exists($title,'director-title')) wp_insert_term($title,'director-title');
+
+    $photoTypes = ['General','Dragon Dance','Annual Performance','Trips','Training','Events','Tournaments'];
+    foreach ($photoTypes as $type) if (!term_exists($type,'gallery-filter')) wp_insert_term($type,'gallery-filter');
+}
+add_action('init', 'ns_add_terms', 20);
+
+// Exclude specific post
+function ns_exclude_post($query) {
     if (!is_admin() && is_page('instructors')) {
-        $excluded = $query->get('post__not_in');
-        
-        if (!is_array($excluded)) {
-            $excluded = array();
-        }
-        $excluded[] = 208; // add post to exclude
+        $excluded = $query->get('post__not_in') ?: [];
+        $excluded[] = 208;
         $query->set('post__not_in', $excluded);
     }
 }
-add_action('pre_get_posts', 'exclude_specific_post_from_queries');
+add_action('pre_get_posts', 'ns_exclude_post');
 
-// Custom Taxonomy: Status
-function registerStatusTaxonomy()
-{
-    $taxonomy = 'ns-status';
-
-    register_taxonomy($taxonomy, array('instructor', 'director'), array(
-        'labels' => array(
-            'name' => 'Status',
-            'singular_name' => 'Status',
-            'add_new_item' => 'Add New Status',
-            'new_item_name' => 'New Status Name',
-        ),
-        'hierarchical' => true,
-        'show_admin_column' => true,
-        'rewrite' => array('slug' => $taxonomy),
-        'show_in_rest' => true,
-    ));
-
-    $defaultStatuses = array('Current', 'Former', 'Student Instructor');
-
-    foreach ($defaultStatuses as $status) {
-        if (!term_exists($status, $taxonomy)) {
-            wp_insert_term($status, $taxonomy);
-        }
-    }
-}
-add_action('init', 'registerStatusTaxonomy');
-
-// Custom Taxonomy: Director Title
-function registerDirectorTitleTaxonomy()
-{
-    $taxonomy = 'director-title';
-
-    register_taxonomy($taxonomy, 'director', array(
-        'labels' => array(
-            'name' => 'Title',
-            'singular_name' => 'Title',
-            'add_new_item' => 'Add New Title',
-            'new_item_name' => 'New Title Name',
-        ),
-        'hierarchical' => true,
-        'show_admin_column' => true,
-        'rewrite' => array('slug' => $taxonomy),
-        'show_in_rest' => true,
-    ));
-
-    $defaultTitle = array('President', 'Vice President', 'Treasurer', 'Secretary', 'Member');
-
-    foreach ($defaultTitle as $title) {
-        if (!term_exists($title, $taxonomy)) {
-            wp_insert_term($title, $taxonomy);
-        }
-    }
-}
-add_action('init', 'registerDirectorTitleTaxonomy');
-
-// Custom Taxonomy: Gallery Filters
-function registerGalleryFilterTaxonomy()
-{
-    $taxonomy = 'gallery-filter';
-
-    register_taxonomy($taxonomy, array('photo'), array(
-        'labels' => array(
-            'name' => 'Photo Types',
-            'singular_name' => 'Photo Type',
-            'add_new_item' => 'Add New Photo Type',
-            'new_item_name' => 'New Photo Type Name',
-        ),
-        'hierarchical' => true,
-        'show_admin_column' => true,
-        'rewrite' => array('slug' => $taxonomy),
-        'show_in_rest' => true, 
-    ));
-
-    $defaultTypes = array('General', 'Dragon Dance', 'Annual Performance', 'Trips', 'Training', 'Events', 'Tournaments');
-
-    foreach ($defaultTypes as $type) {
-        if (!term_exists($type, $taxonomy)) {
-            wp_insert_term($type, $taxonomy);
-        }
-    }
-}
-add_action('init', 'registerGalleryFilterTaxonomy');
-
-// Shortcode
-// Shortcode and Enqueue: Carousel
-function enqueueHeroCarousel()
-{
-    wp_enqueue_script(
-        'heroCarouselJs',
-        get_template_directory_uri() . '/assets/js/carousel/carousel.js',
-        array(),
-        null,
-        true
-    );
-
-    wp_enqueue_style(
-        'hero-carousel-css',
-        get_template_directory_uri() . '/assets/css/carousel.css'
-    );
-}
-add_action('wp_enqueue_scripts', 'enqueueHeroCarousel');
-
-function heroCarouselShortcode()
-{
-    // Grabbing images from ACF field w ID
+// Shortcodes
+// Shortcode: Carousel
+function heroCarouselShortcode() {
     $images = get_field('carousel_images', get_the_ID());
-
-    if (!$images)
-        return '<p>No images found.</p>';
+    if (!$images) return '<p>No images found.</p>';
 
     ob_start(); ?>
-
     <div class="hero-carousel">
-        <?php foreach ($images as $image): ?>
+        <?php foreach ($images as $img): ?>
             <div class="carousel-slide">
-                <img src="<?php echo esc_url(wp_get_attachment_image_url($image['ID'], 'bg')); ?>"
-                    alt="<?php echo esc_attr($image['alt']); ?>" />
+                <img src="<?php echo esc_url(wp_get_attachment_image_url($img['ID'],'bg')); ?>"
+                     alt="<?php echo esc_attr($img['alt']); ?>" />
             </div>
         <?php endforeach; ?>
     </div>
-
     <?php
     return ob_get_clean();
 }
-add_shortcode('hero_carousel', 'heroCarouselShortcode');
-
+add_shortcode('hero_carousel','heroCarouselShortcode');
 
 // Shortcode: Scroll Arrow
-function enqueueScrollArrow()
-{
-    wp_register_script(
-        'scroll-arrow-script',
-        get_template_directory_uri() . '/assets/js/scroll-arrow/scroll-arrow.js',
-        [],
-        '1.0',
-        true
-    );
-}
-add_action('init', 'enqueueScrollArrow');
-
-function scrollArrowShortcode()
-{
-    wp_enqueue_style('scroll-arrow-style');
+function scrollArrowShortcode() {
     wp_enqueue_script('scroll-arrow-script');
+    wp_enqueue_style('scroll-arrow-style');
 
     return '<div id="scroll-arrow-container"></div>';
 }
-add_shortcode('scroll_arrow', 'scrollArrowShortcode');
+add_shortcode('scroll_arrow','scrollArrowShortcode');
 
-// Shortcode: theme toggle for light and dark mode
-function enqueueThemeToggle()
-{
-    wp_register_script(
-        'theme-toggle-script',
-        get_template_directory_uri() . '/assets/js/theme-toggle/theme-toggle.js',
-        [],
-        '1.0',
-        true
-    );
-}
-add_action('init', 'enqueueThemeToggle');
-
-function themeToggleShortcode()
-{
-    wp_enqueue_style('theme-toggle-style');
-    wp_enqueue_script('theme-toggle-script');
-
+// Shortcode: Theme Toggle
+function themeToggleShortcode() {
     return '<button id="theme-toggle-btn" aria-label="Toggle Theme" class="toggleBtn"></button>';
 }
-add_shortcode('theme_toggle', 'themeToggleShortcode');
+add_shortcode('theme_toggle','themeToggleShortcode');
 
+// Other
 // Custom Image Sizes
 add_theme_support('post-thumbnails');
-function custom_image_sizes()
-{
-    // Add custom image sizes
-    add_image_size('small', 150, 150, true);
-    add_image_size('medium', 300, 300, true);
-    add_image_size('large', 600, 600, true);
-    add_image_size('bg', 1920, 1080, true);
-    add_image_size('gallery', 960, 960, true); 
+function ns_custom_image_sizes() {
+    add_image_size('small',150,150,true);
+    add_image_size('medium',300,300,true);
+    add_image_size('large',600,600,true);
+    add_image_size('bg',1920,1080,true);
+    add_image_size('gallery',960,960,true);
 
-    // Add custom sizes to media selector dropdown in WP Admin
-    add_filter('image_size_names_choose', function ($sizes) {
-        return array_merge($sizes, [
-            'small' => __('NS Small'),
-            'medium' => __('NS Medium'),
-            'large' => __('NS Large'),
-            'bg' => __('NS Background'),
-            'gallery' => __('NS Gallery Photo'),
+    add_filter('image_size_names_choose', function($sizes){
+        return array_merge($sizes,[
+            'small'=>'NS Small',
+            'medium'=>'NS Medium',
+            'large'=>'NS Large',
+            'bg'=>'NS Background',
+            'gallery'=>'NS Gallery Photo',
         ]);
     });
 }
-add_action('after_setup_theme', 'custom_image_sizes');
-// To ensure images use BG
-function useCustomImgSize()
-{
-    $group = get_field('group_68705bd0b583a');
+add_action('after_setup_theme','ns_custom_image_sizes');
 
-    if ($group && isset($group['images'])) {
-        foreach ($group['images'] as $image) {
-            if (isset($image['ID'])) {
-                $image_url = wp_get_attachment_image_url($image['ID'], 'bg');
-                echo '<img src="' . esc_url($image_url) . '" alt="' . esc_attr($image['alt']) . '" />';
-            }
-        }
-    }
-}
-// Replace default thumbnail size
-add_filter('render_block', function ($block_content, $block) {
-    if ($block['blockName'] === 'core/post-featured-image') {
-        if (isset($block['attrs']['sizeSlug']) && $block['attrs']['sizeSlug'] === 'thumbnail') {
+// Replace default featured image block with gallery size
+add_filter('render_block', function($content, $block){
+    if($block['blockName']==='core/post-featured-image'){
+        if(isset($block['attrs']['sizeSlug']) && $block['attrs']['sizeSlug']==='thumbnail'){
             $post_id = get_the_ID();
-            $image_id = get_post_thumbnail_id($post_id);
-            if ($image_id) {
-                $image_html = wp_get_attachment_image($image_id, 'gallery', false, [
-                    'class' => 'wp-block-post-featured-image galleryImg',
-                ]);
-                return $image_html;
+            $img_id = get_post_thumbnail_id($post_id);
+            if($img_id){
+                return wp_get_attachment_image($img_id,'gallery',['class'=>'wp-block-post-featured-image galleryImg']);
             }
         }
     }
-    return $block_content;
-}, 10, 2);
+    return $content;
+},10,2);
+
 // Password expire set to 1 hour
-add_filter('post_password_expires', function($expires) {
-    return 3600; 
-});
+add_filter('post_password_expires', fn($expires)=>3600);
